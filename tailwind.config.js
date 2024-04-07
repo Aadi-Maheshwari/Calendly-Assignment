69e130980503046d84af62cbb81f1b6a0c6063dd
Aadi-Maheshwari <151908044+Aadi-Maheshwari@users.noreply.github.com>
2024-04-07 15:17:15 +0530

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,52 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    
+    screens: {
+      
+      'ug': '430px',
+     
+      'md': '768px',
+      
+      'xg': '1030px',
+      
+      'pg': '992px',
+      
+      'tg': '805px',
+       
+      '2xl': '1180px',
+
+      'qg': '565px',
+
+      'hg': '535px',
+
+      'yg': '472px',
+
+      'ig': '410px',
+
+      'gg': '741px',
+
+      'rg': '352px',
+
+      'vg': '1054px',
+
+      'yg': '700px',
+      
+      'bg': '344px',
+    },
+    
+    
+    
+    fontFamily: {
+      poppins: ["Poppins", "sans-serif"],
+     
+    },
+    
+    extend: {
+      boxShadow: {
+        '3xl': '0px 0px 5px rgba(0,0,0,.15);',
+      }
+    },
   },
   plugins: [],
 }
